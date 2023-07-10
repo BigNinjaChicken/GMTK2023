3cd553ec8d2e4b9e6485aad0bc92b6b0dac44c94
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2023-07-10 07:41:13 -0400

    gy

--- Source/GMTK2023/SpearCharacter.h
+++ Source/GMTK2023/SpearCharacter.h
@@ -13,6 +13,8 @@ class GMTK2023_API ASpearCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+public:
+
 	/** Camera boom positioning the camera behind the character */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
